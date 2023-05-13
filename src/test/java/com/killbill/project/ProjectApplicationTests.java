9bf4bedcc845f86c8b7a7935e915bc274098fd8e
sangwonyou <tkddnjs231@naver.com>
2023-05-13 12:13:57 +0900

    feat: project init

---
+++ src/test/java/com/killbill/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.killbill.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
