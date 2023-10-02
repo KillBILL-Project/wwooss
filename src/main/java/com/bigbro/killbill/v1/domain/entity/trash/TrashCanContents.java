0b685c53b0bfd479b5cc1d562dede307a415e47f
sangwonyou <tkddnjs231@naver.com>
2023-10-02 19:47:27 +0900

    feat: 쓰레기통 내용물 생성 api todo : 쓰레기통 내용물 생성 할 떄 로그 데이터도 적재

--- src/main/java/com/bigbro/killbill/v1/domain/entity/trash/TrashCanContents.java
+++ src/main/java/com/bigbro/killbill/v1/domain/entity/trash/TrashCanContents.java
@@ -13,7 +13,8 @@ import javax.persistence.*;
 /**
  * 쓰레기통 로그를 이용 안하고 별도로 쓰레기통 내용물 테이블을 둔 이유
  * 로그를 이용하게 될 경우 이미 비워진 쓰레기 내용물까지 포함하고 있어
- * 나중에 많은 쓰레기 로그가 존재할 경우 조회 성능에 문제 생길걸 대비하여 현 쓰레기통 상태값 만을 위한 값을 별도로 둠.
+ * 나중에 많은 쓰레기 로그가 존재할 경우 조회 성능에 문제 생길걸 대비하여
+ * 현 쓰레기통 상태값 만을 위한 값을 별도로 둠.
  */
 
 @Entity
@@ -39,4 +40,12 @@ public class TrashCanContents extends BaseEntity {
     @OneToOne
     @JoinColumn(name = "trash_info_id")
     private TrashInfo trashInfo;
+
+    public static TrashCanContents of(TrashInfo trashInfo, User user, Long trashCount) {
+        return TrashCanContents.builder()
+                .trashCount(trashCount)
+                .user(user)
+                .trashInfo(trashInfo)
+                .build();
+    }
 }
---
+++ src/main/java/com/bigbro/killbill/v1/service/trash/impl/TrashCanContentsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.bigbro.killbill.v1.service.trash.impl;
+
+import com.bigbro.killbill.v1.common.KillBillResponseCode;
+import com.bigbro.killbill.v1.domain.entity.trash.TrashCanContents;
+import com.bigbro.killbill.v1.domain.entity.trash.TrashInfo;
+import com.bigbro.killbill.v1.domain.entity.user.User;
+import com.bigbro.killbill.v1.domain.request.trash.can.TrashCanContentsRequest;
+import com.bigbro.killbill.v1.exception.DataNotFoundException;
+import com.bigbro.killbill.v1.repository.trash.can.TrashCanContentsRepository;
+import com.bigbro.killbill.v1.repository.trash.info.TrashInfoRepository;
+import com.bigbro.killbill.v1.repository.user.UserRepository;
+import com.bigbro.killbill.v1.service.trash.can.TrashCanContentsService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class TrashCanContentsServiceImpl implements TrashCanContentsService {
+
+    private final UserRepository userRepository;
+
+    private final TrashInfoRepository trashInfoRepository;
+
+    private final TrashCanContentsRepository trashCanContentsRepository;
+
+    @Override
+    @Transactional
+    public void createTrashCanContents(TrashCanContentsRequest trashCanContentsRequest, Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new DataNotFoundException(KillBillResponseCode.NOT_FOUND_DATA, "존재하지 않는 유저입니다."));
+        TrashInfo trashInfo = trashInfoRepository.findById(trashCanContentsRequest.getTrashInfoId()).orElseThrow(() -> new DataNotFoundException(KillBillResponseCode.NOT_FOUND_DATA, "존재하지 않는 쓰레기 정보 입니다."));
+
+        trashCanContentsRepository.save(TrashCanContents.of(trashInfo, user, trashCanContentsRequest.getTrashCount()));
+    }
+}
