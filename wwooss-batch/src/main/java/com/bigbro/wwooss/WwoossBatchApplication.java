31ab469d049f8d59293e6cbcfa20e72ac63d44e9
SongWonny <ysw@fountfinance.com>
2023-11-01 08:38:13 +0900

    fix: component scan

--- wwooss-batch/src/main/java/com/bigbro/wwooss/WwoossBatchApplication.java
+++ wwooss-batch/src/main/java/com/bigbro/wwooss/WwoossBatchApplication.java
@@ -2,8 +2,12 @@ package com.bigbro.wwooss;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableBatchProcessing
+@ComponentScan(basePackages = {"com.bigbro.wwooss", "org.springframework.batch.core.configuration.annotation"})
+@EnableAutoConfiguration
 public class WwoossBatchApplication {
 
     public static void main(String[] args) {
