9034fb354b3a27c5ee1ba016b2abf5a1b56df907
SongWonny <ysw@fountfinance.com>
2023-11-02 08:46:43 +0900

    feat: report 생성 진행중 - weeklyReport repository 생성 - weeklyReport queryDsl 생성 - 생성 로직

--- wwooss-batch/src/main/java/com/bigbro/wwooss/v1/job/WeeklyReportJobConfig.java
+++ wwooss-batch/src/main/java/com/bigbro/wwooss/v1/job/WeeklyReportJobConfig.java
@@ -4,9 +4,16 @@ import static com.bigbro.wwooss.v1.entity.user.QUser.user;
 
 import com.bigbro.wwooss.v1.batch.reader.QuerydslPagingItemReader;
 import com.bigbro.wwooss.v1.entity.report.WeeklyReport;
+import com.bigbro.wwooss.v1.entity.trash.log.TrashLog;
 import com.bigbro.wwooss.v1.entity.user.User;
+import com.bigbro.wwooss.v1.repository.report.WeeklyReportRepository;
 import com.bigbro.wwooss.v1.repository.trash.log.TrashLogRepository;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import javax.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -41,8 +48,10 @@ public class WeeklyReportJobConfig {
 
     private final TrashLogRepository trashLogRepository;
 
+    private final WeeklyReportRepository weeklyReportRepository;
+
     @Bean
-    public Job job() throws Exception {
+    public Job job() {
         return jobBuilderFactory.get(JOB_NAME)
                 .start(step())
                 .build();
@@ -58,15 +67,21 @@ public class WeeklyReportJobConfig {
                 .build();
     }
 
+    /**
+     * 추후 개선 시도
+     * proccessor 제거 후 바로 처리
+     */
     @Bean
     public ItemProcessor<User, WeeklyReport> processor() {
         return new ItemProcessor<User, WeeklyReport>() {
             @Override
-            public WeeklyReport process(User user) throws Exception {
+            public WeeklyReport process(User user) {
                 LocalDate date = LocalDate.parse("2023-11-01");
 
-                trashLogRepository.findTrashLogByUserAtLastWeek(user, date);
-                return null;
+                List<TrashLog> trashLogByUserAtLastWeek = trashLogRepository.findTrashLogByUserAtLastWeek(user, date);
+                WeeklyReport report = createReport(user, trashLogByUserAtLastWeek);
+
+                return Objects.isNull(report) ? null : weeklyReportRepository.save(report);
             }
         };
     }
@@ -84,7 +99,18 @@ public class WeeklyReportJobConfig {
                 .build();
     }
 
-    private WeeklyReport createReport(User user) {
-        return null;
+    private WeeklyReport createReport(User user, List<TrashLog> trashLogList) {
+        if (trashLogList.isEmpty()) {
+            return null;
+        }
+        Set<Integer> attendanceSet = new HashSet<>();
+
+        for(int i = 0; i < trashLogList.size(); i++) {
+            TrashLog trashLog = trashLogList.get(i);
+            attendanceSet.add(trashLog.getCreatedAt().getDayOfWeek().getValue());
+        }
+
+        List<Integer> attendanceList = new ArrayList<>(attendanceSet);
+        return WeeklyReport.of(attendanceList, user);
     }
 }
