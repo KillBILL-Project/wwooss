9ebce294d9ffae1846513d892f3f206e22822db1
SongWonny <ysw@fountfinance.com>
2023-11-17 16:45:10 +0900

    fix: converter 사용 시  querydsl contains 사용 못함

--- wwooss-domain-rds/src/main/java/com/bigbro/wwooss/v1/repository/alarm/custom/AlarmRepositoryCustomImpl.java
+++ wwooss-domain-rds/src/main/java/com/bigbro/wwooss/v1/repository/alarm/custom/AlarmRepositoryCustomImpl.java
@@ -4,7 +4,6 @@ import static com.bigbro.wwooss.v1.entity.alarm.QAlarm.alarm;
 
 import com.bigbro.wwooss.v1.entity.alarm.Alarm;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.time.DayOfWeek;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +18,6 @@ public class AlarmRepositoryCustomImpl implements AlarmRepositoryCustom {
         return queryFactory.selectFrom(alarm)
                 .where(alarm.sendHour.eq(hour)
                         .and(alarm.sendMinute.eq(minute))
-                        .and(alarm.dayOfWeekList.contains(day))
                         .and(alarm.isOn.eq(true)))
                 .fetch();
     }
