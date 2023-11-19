f5087df7e442e6468e324bfb5fdfe086404277ad
SongWonny <ysw@fountfinance.com>
2023-11-20 07:10:22 +0900

    feat: 알람 조회 수정

--- wwooss-batch/src/main/java/com/bigbro/wwooss/v1/job/AlarmTasklet.java
+++ wwooss-batch/src/main/java/com/bigbro/wwooss/v1/job/AlarmTasklet.java
@@ -21,19 +21,30 @@ public class AlarmTasklet implements Tasklet, StepExecutionListener {
 
     private List<Alarm> alarmList;
 
+    private int dayOfWeek;
+
     private final AlarmRepository alarmRepository;
 
     @Override
     public void beforeStep(StepExecution stepExecution) {
         String date = stepExecution.getJobParameters().getParameters().get("date").toString();
         LocalDateTime parseDate = parse(date, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        dayOfWeek = parseDate.getDayOfWeek().getValue();
+
         alarmList = alarmRepository.findAlarmAtTime(parseDate.getHour(), parseDate.getMinute(),
                 parseDate.getDayOfWeek().getValue());
     }
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        alarmList.stream()
+                .filter((alarm) -> alarm.getDayOfWeekList().contains(dayOfWeek))
+                .forEach((this::sendPushNotification));
         return RepeatStatus.FINISHED;
+    }
+
+    private void sendPushNotification(Alarm alarm) {
+
     }
 
     @Override
--- wwooss-domain-rds/src/main/java/com/bigbro/wwooss/v1/repository/alarm/custom/AlarmRepositoryCustomImpl.java
+++ wwooss-domain-rds/src/main/java/com/bigbro/wwooss/v1/repository/alarm/custom/AlarmRepositoryCustomImpl.java
@@ -1,6 +1,7 @@
 package com.bigbro.wwooss.v1.repository.alarm.custom;
 
 import static com.bigbro.wwooss.v1.entity.alarm.QAlarm.alarm;
+import static com.bigbro.wwooss.v1.entity.user.QUser.user;
 
 import com.bigbro.wwooss.v1.entity.alarm.Alarm;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -16,9 +17,11 @@ public class AlarmRepositoryCustomImpl implements AlarmRepositoryCustom {
     @Override
     public List<Alarm> findAlarmAtTime(int hour, int minute, int day) {
         return queryFactory.selectFrom(alarm)
+                .join(alarm.user, user)
                 .where(alarm.sendHour.eq(hour)
                         .and(alarm.sendMinute.eq(minute))
-                        .and(alarm.isOn.eq(true)))
+                        .and(alarm.isOn.eq(true))
+                        .and(user.pushConsent.eq(true)))
                 .fetch();
     }
 }
