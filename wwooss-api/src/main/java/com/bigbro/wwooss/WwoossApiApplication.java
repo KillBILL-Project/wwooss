411042a0556411e94b44bb4f7a488b4b74e3d510
SongWonny <ysw@fountfinance.com>
2024-01-12 16:32:51 +0900

    fix: 잘못된 yaml import

--- wwooss-api/src/main/java/com/bigbro/wwooss/WwoossApiApplication.java
+++ wwooss-api/src/main/java/com/bigbro/wwooss/WwoossApiApplication.java
@@ -1,11 +1,12 @@
 package com.bigbro.wwooss;
 
+import com.bigbro.wwooss.v1.factory.YamlPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource({"classpath:application-rds.yaml"})
+@PropertySource(value = "classpath:application.yaml", factory = YamlPropertySourceFactory.class)
 public class WwoossApiApplication {
 
     public static void main(String[] args) {
--- wwooss-batch/src/main/java/com/bigbro/wwooss/WwoossBatchApplication.java
+++ wwooss-batch/src/main/java/com/bigbro/wwooss/WwoossBatchApplication.java
@@ -1,13 +1,16 @@
 package com.bigbro.wwooss;
 
+import com.bigbro.wwooss.v1.factory.YamlPropertySourceFactory;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableBatchProcessing
 @EnableScheduling
 @SpringBootApplication
+@PropertySource(value = "classpath:application-batch.yaml", factory = YamlPropertySourceFactory.class)
 public class WwoossBatchApplication {
 
     public static void main(String[] args) {
